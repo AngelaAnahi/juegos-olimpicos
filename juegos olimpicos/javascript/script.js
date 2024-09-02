092d546ead3696fc191982a04ffbf298f845bec2
AngelaAnahi <166661141+AngelaAnahi@users.noreply.github.com>
2024-09-01 23:48:14 -0300

    Add files via upload

--- juegos olimpicos/javascript/script.js
+++ juegos olimpicos/javascript/script.js
@@ -1,5 +1,33 @@
-let nombre = "Angi"
-let numero = "19"
-let verdaderoOFalso = true
+let nombre = "Angi";
+let numero = "19";
+let verdaderoOFalso = true;
 
-nombre = "Angela"
+nombre = "Angela";
+
+function sumatoria(num1, num2){
+    let resultado = num1 + num2;
+    console.log(resultado);
+}
+
+sumatoria(151, 240);
+
+function nombres(nombre){
+    console.log("Hola," + nombre + " Como estas?");
+}
+
+nombres("Lorenzo");
+
+function mtsCuadrados (largo, ancho){
+    let mts = largo * ancho;
+    console.log(mts + "m2");
+}
+
+mtsCuadrados(6,1);
+mtsCuadrados(3,5);
+
+function area(num1, num2){
+     let resultado = num1 + num2;
+     console.log(resultado)
+}
+
+area(3.14,1.784)
